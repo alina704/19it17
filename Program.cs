d808bce15e7cfa9ef7a4ea342781b13b6211a41f
Alina <alinaabraskina73@gmail.com>
2020-02-05 11:09:21 +0300

    test

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace rfghjj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            int k = 0;
+            int first;
+            int N = int.Parse(Console.ReadLine());
+            int[] arr = new int[N];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < N; i++)
+            {
+                arr[i] = rnd.Next(0, 10000);
+            }
+
+            
+            for (int i = 0; i < N; i++)
+            {
+                if (arr[i] > max) { 
+                    max = arr[i];
+                    k = arr[0];
+                    arr[i] = k;
+                    arr[0] = max;
+                }
+            }
+            first = max;
+            Console.WriteLine(first);
+            foreach(int i in arr)
+            {
+                Console.Write(i + "");
+            }
+        }
+
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace dgfhj2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 0;
+            int g = 0;
+            int max = 0;
+            Console.Write("Введите N : ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            while (N > g)
+            {
+                Random random = new Random();
+                int[] arr = new int[N - g];
+                for (int i = 0; i < N - g; i++)
+                {
+                    arr[i] = random.Next(0, 1000);
+                }
+
+
+                Console.WriteLine("Исходный массив:");
+                foreach (int x in arr)
+                {
+                    Console.Write(x + " ");
+                }
+
+                max = arr[g];
+
+                for (int i = g; i < arr.Length; i++)
+                {
+                    if (arr[i] > max)
+                    {
+                        max = arr[i];
+                        k = arr[g];
+                        arr[i] = k;
+                        arr[0] = max;
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Полученный массив:");
+                foreach (int i in arr)
+                {
+                    Console.Write(i + " ");
+                }
+
+                N--;
+            }
+            Console.ReadKey();
+        }
+    }
+}
+        
+    
+
