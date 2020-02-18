0db4f17e6814a9f8b110cbc9873ae3ac132d76e6
Alina <alinaabraskina73@gmail.com>
2020-02-18 09:10:20 +0300

    chastota cs

---
+++ Chastota.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+ 
+public class Chastota
+{
+    public static void Main(String[] args)
+    {
+        List<Int16> Virables = new List<Int16>();
+
+        for (Int32 i = Int32.Parse(Console.ReadLine()); i > 0; i--)
+            Virables.Add(Int16.Parse(Console.ReadLine()));
+
+        foreach (var Line in from g in Virables
+                             group g by g into ng
+                             orderby ng.Key
+                             select new
+                             {
+                                 Count = ng.Count(),
+                                 Key = ng.Key
+                             })
+            Console.WriteLine("{0} {1}", Line.Key, Line.Count);
+    }
+}
